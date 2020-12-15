d6b46211b5e107477ec58d5d063230dfd9b900d3
ixiaoyy <phpxiaoyz@163.com>
2020-12-15 09:54:15 +0800

    修复前端

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -4,5 +4,4 @@ Vue.use(vueBaberrage);
 
 require('./bootstrap');
 
-Vue.component('tasks-component', require('./components/TasksComponent.vue').default);
 Vue.component('danmu-component', require('./components/DanmuComponent.vue').default);
