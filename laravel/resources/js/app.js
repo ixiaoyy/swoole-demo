29d954ae947c146c4b0161e5ad2dfeceb4895497
ixiaoyy <phpxiaoyz@163.com>
2020-12-15 09:51:33 +0800

    弹幕-前端

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,8 @@
+window.Vue = require('vue');
+import {vueBaberrage} from 'vue-baberrage';
+Vue.use(vueBaberrage);
+
 require('./bootstrap');
+
+Vue.component('tasks-component', require('./components/TasksComponent.vue').default);
+Vue.component('danmu-component', require('./components/DanmuComponent.vue').default);
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -22,3 +22,7 @@ Route::get('/task/test', function () {
     $success = \Hhxsv5\LaravelS\Swoole\Task\Task::deliver($task);  // 异步投递任务，触发调用任务类的 handle 方法
     var_dump($success);
 });
+
+Route::get('/danmu', function() {
+    return view('danmu');
+});
