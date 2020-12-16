94dd872570be042e7bc7c610243953b30477f743
ixiaoyy <phpxiaoyz@163.com>
2020-12-16 17:54:04 +0800

    arithmetic

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -3,3 +3,7 @@ import {vueBaberrage} from 'vue-baberrage';
 Vue.use(vueBaberrage);
 
 Vue.component('danmu-component', require('./components/DanmuComponent.vue'));
+
+new Vue({
+    el: 'danmu-component'
+});
