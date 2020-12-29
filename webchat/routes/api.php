f3dacecfc6838293ca64be021817b60eab3d1725
ixiaoyy <phpxiaoyz@163.com>
2020-12-29 15:00:51 +0800

    修改路由

--- webchat/routes/api.php
+++ webchat/routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/register', 'AuthController@register');
-Route::post('/login', 'AuthController@login');
+Route::middleware('auth:api')->post('/register', 'AuthController@register');
+Route::middleware('auth:api')->post('/login', 'AuthController@login');
